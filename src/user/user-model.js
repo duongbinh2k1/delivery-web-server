c58c541f0a6378be2e6319e1a692c914e4739f55
Duong Tan Binh <v.binhdt26@vinbigdata.com>
2023-03-10 17:49:14 +0700

    update init verify email

--- src/user/user-model.js
+++ src/user/user-model.js
@@ -3,36 +3,39 @@ const bcrypt = require('bcrypt')
 
 const { userVariables } = require('./user-validate')
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    min: userVariables.minUsernameLength,
-    max: userVariables.maxUsernameLength,
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: userVariables.minUsernameLength,
+      max: userVariables.maxUsernameLength,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      min: userVariables.minFullNameLength,
+      max: userVariables.maxFullNameLength,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: userVariables.maxMailLength,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: userVariables.minPasswordLength,
+      max: userVariables.maxPasswordLength,
+    },
+    inviteCode: {
+      type: String,
+      required: false,
+      length: userVariables.inviteCodeLength,
+    },
   },
-  fullName: {
-    type: String,
-    required: true,
-    min: userVariables.minFullNameLength,
-    max: userVariables.maxFullNameLength,
-  },
-  email: {
-    type: String,
-    required: true,
-    max: userVariables.maxMailLength,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: userVariables.minPasswordLength,
-    max: userVariables.maxPasswordLength,
-  },
-  inviteCode: {
-    type: String,
-    required: false,
-    length: userVariables.inviteCodeLength,
-  }
-})
+  { timestamps: true }
+);
 
 userSchema.pre("save", async function (next) {
   try {
